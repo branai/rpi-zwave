80166aeb438677a5cd72bd04d5ebc829aef5be04
branai <bijanrnaimi@gmail.com>
2017-11-10 14:31:04 -0800

    updated framework of handel.json

--- client/app.js
+++ client/app.js
@@ -1,17 +1,7 @@
-var connect = require('./updateRPI');
-var dns = require('dns');
-var os = require('os');
-//TODO: make method to get client's mac address (unique)
-var myip;
-dns.lookup(os.hostname(), function (err, add, fam) {
-    myip = add;
-    endLookup();
-})
+//View state with ftp://IPADDRESS:7002/public/state.json
 
-function endLookup() {
-  //want should be true:get values, false:ignore values
-  var want = true;
-  connect.send(want, myip);
-}
+var connect = require('./updateRPI');
+var want = true;
+connect.send(want);
 
 //TODO: make method for reading the state.json on rpi
--- client/updateRPI.js
+++ client/updateRPI.js
@@ -1,21 +1,30 @@
 var ftpClient = require('ftp-client');
 var fs = require('fs');
+var os = require('os');
+var network = os.networkInterfaces();
 var config = {
-    host: '10.0.0.27', //<-- ip to ftp to
+    host: '10.252.35.9', //<-- ip to ftp to
     port: 7002 //<-- ftp port server is taking ftp requests
 }
 
 var options = {
     logging: 'basic'
 }
+
+var myInfo = {
+    ip: network.en0[1]['address'],
+    ma: network.en0[1]['mac']
+}
+
 //Run protocol that sends the protocol and some info (main.js)
-exports.send = function(protocol,info) {
+exports.send = function(protocol) {
     //Read 'mail/handel.json'
     var obj1 = function () {return(JSON.parse(fs.readFileSync('mail/handel.json')))};
     var obj = obj1();
     //Update 'mail/handel.json' info
     obj['protocol'] = protocol;
-    obj['info'] = info;
+    obj['info'] = myInfo;
+    console.log(obj);
     //Update 'mail/handel.json'
     var json = JSON.stringify(obj);
     fs.writeFile('mail/handel.json', json);
