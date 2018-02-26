07faf9f0201e1d99e7db66533a27a5d7a1f7b450
EC2 Default User <ec2-user@ip-172-31-3-172.us-west-1.compute.internal>
2018-02-26 05:18:19 +0000

    changed ftp module; must verify on server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
-const FtpSrv = require('ftp-srv');
+const FtpServ = require('ftp-srv');
 var decr = require('./decr.js');
-var fs = require('fs');
-const ftpServer = new FtpSrv('ftp://54.193.44.245:7001');
-ftpServer.on('login', ({connection}, resolve) => {
-  resolve({root:'public/'})
-  connection.on('STOR', (error, fileName) => {
-     if(fileName == 'container/state.txt'){
-       try{
-         var str = JSON.parse(decr.decrypt(fs.readFileSync(fileName)));
-         fs.writeFile('state.txt', str);
-       } catch (e) {}
-   });
+const ftpServer = new FtpServ('ftp://54.193.44.245:7001');
+ftpServer.on('login', function({connection}, resolve) {
+  resolve({root:'public'})
+  var fs = require('fs');
+  connection.on('STOR', function(error, fileName) { 
+     if(fileName == 'container/state.txt' && fs.existsSync('public/container/state.txt')){
+       var str = fs.readFileSync('public/container/state.txt');
+       //check if this works to validate -->JSON.parse(decr.decrypt(str));
+       fs.writeFile('public/safe/state.txt', str);
+       fs.writeFile('safe.txt', str);
+     }
+ });
 });
 ftpServer.listen();
+
