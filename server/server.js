f8011e86c92ce299c767b84143caf848319a06cc
EC2 Default User <ec2-user@ip-172-31-3-172.us-west-1.compute.internal>
2018-03-05 06:23:45 +0000

    implemented http

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var http = require('http');
+var safe = "this should not be here";
 var fs = require('fs');
 var port = 7001;
 var querystring  = require('querystring');
 var server = http.createServer(function(request, response) {
   if(request.method != "POST") {
-    response.end(fs.readFileSync("state.txt"));
+    response.end(safe.toString());
     return;
   }
   var body = '';
@@ -15,27 +16,10 @@ var server = http.createServer(function(request, response) {
   request.on('end', function () {
     var post = querystring.parse(body);
     //TODO: check if it is parseable
-    fs.writeFileSync("state.txt", post);
+    safe = post.str;
+    console.log(post);
   });
   response.end();
 });
 
 server.listen(port, function(err) { console.log(`server is listening on ${port}`) });
-
-/*const FtpServ = require('ftp-srv');
-var decr = require('./decr.js');
-const ftpServer = new FtpServ('ftp://54.193.44.245:7001');
-ftpServer.on('login', function({connection}, resolve) {
-  resolve({root:'public'})
-  var fs = require('fs');
-  connection.on('STOR', function(error, fileName) {
-     if(fileName == 'container/state.txt' && fs.existsSync('public/container/state.txt')){
-       var str = fs.readFileSync('public/container/state.txt');
-       //check if this works to validate -->JSON.parse(decr.decrypt(str));
-       fs.writeFile('public/safe/state.txt', str);
-       fs.writeFile('safe.txt', str);
-     }
- });
-});
-ftpServer.listen();
-*/
