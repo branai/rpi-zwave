ebd03cfa1ef266ae148ae50c0b21a8a8f0e008e8
branai <bijanrnaimi@gmail.com>
2018-03-04 23:03:22 -0800

    -

--- client/take.js
+++ client/take.js
@@ -1,38 +1,13 @@
-var ftpClient = require('ftp-client');
+var http = require('http');
 var fs = require('fs');
-var os = require('os');
-var cryp = require('./decr.js');
-var config = {
-    host: '54.193.44.245', //<-- ip to ftp to
-    port: 7001 //<-- ftp port server is taking ftp requests
-}
-
-var options = {
-    logging: 'basic'
-}
-
-var myInfo = {
-    ip: 'ip'/*network.en0[1]['address']*/,
-    ma: 'ma'/*network.en0[1]['mac']*/
-
-}
+var decr = require('./decr.js');
+var file = fs.createWriteStream('file.txt');
+http.get("http://54.193.44.245:7001", response => {
+  response.on('data', function(chunk) {
+    var stateDec = decr.decrypt(chunk.toString());
+    fs.writeFileSync('state.json', stateDec);
+    //return(stateDec);
+  });
+}).on('error', function(e){ console.log(e.message); });
 
 
-  //This client connection can be moved once more features are added
-    var client = new ftpClient(config, options);
-    client.connect(function () {
-        client.download('safe', 'notValidated', {
-            overwrite: 'all'
-        }, function (result) {
-            console.log(result);
-            var str = fs.readFileSync('notValidated/state.txt').toString();
-            try {
-              JSON.parse(cryp.decrypt(str));
-            } catch(e) {
-              console.log(e +"====="+str)
-              return;
-            }
-            var jsonString = fs.writeFile('state.json',cryp.decrypt(str));
-        });
-    });
-    //This client connection can be moved once more features are added
