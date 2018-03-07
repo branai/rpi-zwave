58142b1481fd3729788e4be08aa0dae66e693ad7
branai <30612689+branai@users.noreply.github.com>
2018-03-07 00:05:33 -0800

    -

--- server/decr.js
+++ server/decr.js
@@ -1,3 +1,4 @@
+var aesjs = require('aes-js');
 exports.decrypt = function(bytes){
 var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
 var aesCtr = new aesjs.ModeOfOperation.ctr(key);
