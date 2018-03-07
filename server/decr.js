1a09f700267c8c46b7ffc1287302d84b06201cdd
branai <30612689+branai@users.noreply.github.com>
2018-03-07 00:04:56 -0800

    changed crypto module again

--- server/decr.js
+++ server/decr.js
@@ -1,9 +1,7 @@
-var crypto = require("crypto-js");
-var path = require("path");
-var fs = require("fs");
-
-exports.decrypt = function(text) {
-  var key = fs.readFileSync('key.txt');
-  var decipher = crypto.AES.decrypt('aes-256-ctr',key).toString(crypto.enc.Utf8);
-  return decipher;
+exports.decrypt = function(bytes){
+var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
+var aesCtr = new aesjs.ModeOfOperation.ctr(key);
+var decryptedBytes = aesCtr.decrypt(bytes);
+var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+return(decryptedText);
 }
