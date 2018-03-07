1fbf8b8816b29b83d9ac46fe31f0345e042a216a
EC2 Default User <ec2-user@ip-172-31-3-172.us-west-1.compute.internal>
2018-03-07 06:55:16 +0000

    changed crypto module

--- server/decr.js
+++ server/decr.js
@@ -1,10 +1,9 @@
-var crypto = require("crypto");
+var crypto = require("crypto-js");
 var path = require("path");
 var fs = require("fs");
 
 exports.decrypt = function(text) {
   var key = fs.readFileSync('key.txt');
-  var decipher = crypto.createDecipher('aes-256-ctr',key)
-  var stateDec = decipher.update(text,'hex','utf8')
-  return stateDec;
+  var decipher = crypto.AES.decrypt('aes-256-ctr',key).toString(crypto.enc.Utf8);
+  return decipher;
 }
