570e26381c4b5e9a590a8f8db50cb6c4dd3822bc
branai <30612689+branai@users.noreply.github.com>
2018-03-07 19:31:29 -0800

    Update decr.js

--- server/decr.js
+++ server/decr.js
@@ -1,23 +1,5 @@
-var crypto = require('crypto');
-exports.decrypt = function(text1){
-    var algorithm = 'aes-256-ctr';
-    var password = 'hello';
-
-function encrypt(text){
-  var cipher = crypto.createCipher(algorithm,password)
-  var crypted = cipher.update(text,'utf8','hex')
-  crypted += cipher.final('hex');
-  return crypted;
-}
- 
-function decrypt(text){
-  var decipher = crypto.createDecipher(algorithm,password)
-  var dec = decipher.update(text,'hex','utf8')
-  dec += decipher.final('utf8');
-  return dec;
-}
-
- 
-return(decrypt(text1));
-
+var CryptoJS = require('crypto-js');
+exports.decrypt = function(text){
+  var bytes  = CryptoJS.AES.decrypt(text, 'THISISTHEKEY');
+  return(bytes.toString(CryptoJS.enc.Utf8));
 }
