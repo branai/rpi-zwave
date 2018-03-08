9423223477d6d0793d24f1c437be9e08e2218c26
branai <30612689+branai@users.noreply.github.com>
2018-03-07 19:35:51 -0800

    Update encr.js

--- rpi/encr.js
+++ rpi/encr.js
@@ -1,6 +1,4 @@
-var aes256 = require('aes256');
-exports.encrypt = function(text){
-  var key = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCTi32Lm+SR+GDza8LquZ9XKLygViElmYgmiAmzhwZWrM7t+OUn1DWrIO6aSiWflNp7E3k4IK13uWKXI9oadBfImIy3dmocrJXi8tOhWN1hDLIQgFisRF4RcLJGUipzUsqHrXeDOenPfs0Q7M+fzY00CJHvfNsZRU3N7ibChjhWNwIDAQAB';
-  var encrypted = aes256.encrypt(key, text);
-  return(encrypted)
+var CryptoJS = require('crypto-js');
+exports.encrypt(text){
+  return(CryptoJS.AES.encrypt(text, 'THISISTHEKEY'));
 }
