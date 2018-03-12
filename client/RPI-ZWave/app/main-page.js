19669732747a6110208cb875b59409368cbcd31b
branai <30612689+branai@users.noreply.github.com>
2018-03-11 21:59:46 -0700

    Update main-page.js

--- client/RPI-ZWave/app/main-page.js
+++ client/RPI-ZWave/app/main-page.js
@@ -1,13 +1,16 @@
 var http = require('http');
 var decr = require('./decr.js');
-
+var failCount = 0;
 var pulledJSON;
 var download = function() {
   http.getString("http://54.193.44.245:7001").then(function (r) {
     try {
       pulledJSON = JSON.parse(decr.decrypt(r.toString()));
-      console.log("GOOD FILE")
-    } catch(e) {alert("BAD FILE");}
+      console.log("GOOD FILE");
+      failCount = 0;
+    } catch(e) {
+      console.log("BAD FILE");
+    }
   }, function (e) {
      console.log(e.message);
   });
@@ -19,6 +22,10 @@ exports.switched = function(args){
   if(!args.object.checked){
     runDownload = setInterval(function() {
       download();
+      if(failCount > 10) {
+        alert('DETECTED ATTACK');
+        return;
+      }
       if(lastChecked == undefined || pulledJSON == undefined) lastChecked = pulledJSON;
       console.log(lastChecked, pulledJSON)
       try {
