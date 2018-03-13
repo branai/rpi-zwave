ba03d351ee7f631e03c11d06bc935435f74be5bb
branai <bijanrnaimi@gmail.com>
2018-03-12 19:16:09 -0700

    fixed client app

--- client/RPI-ZWave/platforms/ios/RPIZWave/app/main-page.js
+++ client/RPI-ZWave/platforms/ios/RPIZWave/app/main-page.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var decr = require('./decr.js');
 var failCount = 0;
-var pulledJSON;
+var pulledJSON = {'nodes':[]};
 var download = function() {
   http.getString("http://54.193.44.245:7001").then(function (r) {
     try {
@@ -16,9 +16,9 @@ var download = function() {
   });
 }
 
-var lastChecked;
+var lastChecked = {'nodes':[]};
 var runDownload;
-var initSwitch = false;
+var initSwitch = 0;
 exports.switched = function(args){
   if(!args.object.checked){
     runDownload = setInterval(function() {
@@ -27,27 +27,26 @@ exports.switched = function(args){
         alert('DETECTED ATTACK');
         return;
       }
-      if(lastChecked == undefined || pulledJSON == undefined) lastChecked = pulledJSON;
-      console.log(lastChecked, pulledJSON)
-      try {
+        if(pulledJSON['nodes'].length != lastChecked['nodes'].length) lastChecked = pulledJSON;
         for(var i = 1; i < pulledJSON['nodes'].length; i++){
           if(pulledJSON['nodes'][i]['lastTriggerDate'] != lastChecked['nodes'][i]['lastTriggerDate']){
-            if(initSwitch){
-              alert("There was either an alarm trip just now or an alarm trip while you were gone.");
-              initSwitch = false;
+            if(initSwitch == 2){
+              alert("There was either an alarm trip while you were gone.");
             } else {
               alert("ALARM TRIP");
             }
             lastChecked = pulledJSON;
-            break; 
+            break;
           }
         }
-      } catch(e) {
-     
-      }
+        if(initSwitch == 1){
+          initSwitch = 2;
+        } else {
+          initSwitch = 0;
+        }
     }, 1000);
   } else {
-    initSwitch = true;
+    initSwitch = 1;
     lastChecked = pulledJSON;
     clearInterval(runDownload);
   }
--- client/RPI-ZWave/platforms/ios/build/emulator/RPIZWave.app/app/main-page.js
+++ client/RPI-ZWave/platforms/ios/build/emulator/RPIZWave.app/app/main-page.js
@@ -1,13 +1,16 @@
 var http = require('http');
 var decr = require('./decr.js');
-
+var failCount = 0;
 var pulledJSON;
 var download = function() {
   http.getString("http://54.193.44.245:7001").then(function (r) {
     try {
       pulledJSON = JSON.parse(decr.decrypt(r.toString()));
-      console.log("GOOD FILE")
-    } catch(e) {alert("BAD FILE");}
+      console.log("GOOD FILE");
+      failCount = 0;
+    } catch(e) {
+      console.log("BAD FILE");
+    }
   }, function (e) {
      console.log(e.message);
   });
@@ -15,24 +18,36 @@ var download = function() {
 
 var lastChecked;
 var runDownload;
+var initSwitch = false;
 exports.switched = function(args){
   if(!args.object.checked){
     runDownload = setInterval(function() {
       download();
+      if(failCount > 10) {
+        alert('DETECTED ATTACK');
+        return;
+      }
       if(lastChecked == undefined || pulledJSON == undefined) lastChecked = pulledJSON;
       console.log(lastChecked, pulledJSON)
       try {
         for(var i = 1; i < pulledJSON['nodes'].length; i++){
           if(pulledJSON['nodes'][i]['lastTriggerDate'] != lastChecked['nodes'][i]['lastTriggerDate']){
-            alert("ALARM TRIP")
+            if(initSwitch){
+              alert("There was either an alarm trip just now or an alarm trip while you were gone.");
+              initSwitch = false;
+            } else {
+              alert("ALARM TRIP");
+            }
             lastChecked = pulledJSON;
+            break; 
           }
         }
       } catch(e) {
-
+     
       }
     }, 1000);
   } else {
+    initSwitch = true;
     lastChecked = pulledJSON;
     clearInterval(runDownload);
   }
